d60f2509983bca34f88eb7d9b384fe608eda2eae
dani <danieharish@gmail.com>
2023-12-15 20:29:54 +0530

    contactupdate

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -68,7 +68,7 @@ const Contact = () => {
               placeholder="Your Message" 
               name="message"
             />
-            <div className="my-button bg-gray-700 hover:bg-gray-500 text-gray-800 border border-gray-500 px-2 py-1 rounded-md transition duration-300 cursor-pointer"><input style={{cursor:"pointer",color:"white"}} type="submit" value="Send" /></div>
+            <button className="my-button bg-gray-700 hover:bg-gray-500 text-gray-800 border border-gray-500 px-2 py-1 rounded-md transition duration-300 cursor-pointer" ><input style={{cursor:"pointer",color:"white"}} type="submit" value="Send" /></button>
             
             </form>
           <div className="flex flex-col  gap-7 ">
